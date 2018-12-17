8e73a9066657486a837d8fde57d1191e302bf3c8
unknown <colonelspacepenguin@gmail.com>
2018-12-17 19:36:44 +0800

    reactsurvey

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Usurvey from './Usurvey';
 
 class App extends Component {
   render() {
@@ -8,9 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+            <Usurvey />
           <a
             className="App-link"
             href="https://reactjs.org"
