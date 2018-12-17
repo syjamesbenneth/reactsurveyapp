6a3263d102c4b6c89b9905310b69ac0b820d27df
JB Sy <42855187+syjamesbenneth@users.noreply.github.com>
2018-12-17 19:45:01 +0800

    Add files via upload

---
+++ public/precache-manifest.37a03f08c952d0d2b47a6dc8d21556b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dad1336e583274da87c0",
+    "url": "/static/js/main.dad1336e.chunk.js"
+  },
+  {
+    "revision": "8d290a0d106d6895454e",
+    "url": "/static/js/1.8d290a0d.chunk.js"
+  },
+  {
+    "revision": "dad1336e583274da87c0",
+    "url": "/static/css/main.2bb6b922.chunk.css"
+  },
+  {
+    "revision": "9abe027be46c0836a57772602db432b4",
+    "url": "/index.html"
+  }
+];
